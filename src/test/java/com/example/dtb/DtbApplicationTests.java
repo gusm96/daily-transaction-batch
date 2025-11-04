3da489f2ffb98d8c876110d681f0880ea394b17e
Seongmo Gu <gusm96@naver.com>
2025-11-04 18:09:25 +0900

    Initial commit

---
+++ src/test/java/com/example/dtb/DtbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
